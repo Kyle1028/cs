5547bf5d4ac6e34022e8cc6c47c8e8304bb6e7cf
Kyle1028 <93895301+Kyle1028@users.noreply.github.com>
2022-12-29 10:49:43 +0800

    Create main1.c

---
+++ HW7/main1.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int i =7;
+    
+    for(int n=1;n<=i;n++){
+        for(int p=1;p<=i;p++){
+            if(p<=i-n){
+                printf(" ");
+            }
+            else{
+                printf("%d ",n);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
