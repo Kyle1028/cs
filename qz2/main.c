a60e25d039fae580c51eb1679bea677cc90bb4e9
Kyle1028 <93895301+Kyle1028@users.noreply.github.com>
2022-12-29 16:55:00 +0800

    Update main.c

--- qz2/main.c
+++ qz2/main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+int rows = 5;
 
+void print_spaces(int r,int rows){
+    for (int r=1;r <= rows ; r++){
+    printf(" ");
+    }
+}
+void print_stars(int k){
+     for(int k=9-(rows*2);k>=1;k--)
+   {
+    printf("*");
+   } 
+}
+int main(){
+    int rows = 5;
+    for(int i = 0; i <=rows;i++){
+        print_spaces(i);
+        print_stars(i);
+        printf("\n");
+    }
+    
+    return 0;
+}
