f3ee179ebd388cd0c718555198594344b8455f14
Kyle1028 <93895301+Kyle1028@users.noreply.github.com>
2023-01-02 21:52:32 +0800

    Create main2.c

---
+++ HW8/main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int bitcount();
+int main(){
+    int x = 0, i;
+    i = 15;
+    x = bitcount(i);
+    printf("%d有%d個1\n\n", i, x);
+
+    return 0;
+}
+int bitcount (int n)
+{
+   int x = 0 ;
+   while (n)
+   {
+      x++ ;
+      n &= (n - 1) ; 
+   }
+   return x ;
+}
